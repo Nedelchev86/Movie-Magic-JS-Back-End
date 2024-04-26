e1bb86da51a1e5db8a8b9d082eafb8835a2f4635
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2024-04-26 20:11:43 +0300

    Add 404

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
 
 const homeController = require("./contollers/homeController");
-const movieContoller = require("./contollers/movieController")
+const movieContoller = require("./contollers/movieController");
 
 router.use(homeController);
-router.use(movieContoller)
+router.use(movieContoller);
+router.all("*", (req, res) => {
+    res.render("404");
+});
 
 module.exports = router;
